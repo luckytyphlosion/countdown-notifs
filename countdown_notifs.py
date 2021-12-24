e202409255fefc8b2e0548ec9a7ef25e9d2a52e8
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2021-12-24 16:04:26 -0500

    Transition to class based notifier.

--- countdown_notifs.py
+++ countdown_notifs.py
@@ -28,11 +28,58 @@ import traceback
 import time
 import subprocess
 import datetime
+from abc import ABC, abstractmethod
 
 CHADSOFT_STATUS_URL = "https://www.chadsoft.co.uk/online-status/status.txt"
 countdown_status_regex = re.compile(r"<h1>COUNTDOWN STATUS: ([^<]+)</h1>")
 
 good_chars = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789., ")
+
+class CountdownNotifier(ABC):
+    __slots__ = ("prev_online_status",)
+
+    def __init__(self):
+        super().__init__()
+        self.prev_online_status = ""
+
+    def wait_status_change(self):
+        exception_sleep_time = 15
+        while True:
+            try:
+                r = requests.get(CHADSOFT_STATUS_URL)
+                if r.status_code == 200:
+                    if r.text != "":
+                        response_text = r.text
+                        match_obj = countdown_status_regex.search(response_text)
+                        if not match_obj:
+                            print("Warning: Could not find online status message.")
+                        else:
+                            online_status = match_obj.group(1)
+                            if online_status != self.prev_online_status:
+                                self.on_status_change(online_status)
+                                self.prev_online_status = online_status
+    
+                        sleep_time = 180
+                    else:
+                        print(f"Chadsoft status returned empty!")
+                        sleep_time = 30
+                else:
+                    print(f"chadsoft returned {r.status_code}: {r.reason}")
+                    sleep_time = 180
+    
+                exception_sleep_time = 15
+                time.sleep(sleep_time)
+            except Exception as e:
+                print(f"Exception occurred: {e}\n{''.join(traceback.format_tb(e.__traceback__))}\nSleeping for {exception_sleep_time} seconds now.")
+                time.sleep(exception_sleep_time)
+                exception_sleep_time *= 2
+                if exception_sleep_time > 1000:
+                    print("Exception happened too many times! Exiting.")
+                    return
+
+    @abstractmethod
+    def on_status_change(self):
+        pass
 
 def keep_only_safe_chars(s):
     return "".join([c for c in s if c in good_chars])
@@ -42,44 +89,26 @@ def send_notification(wav_file, status):
     #subprocess.run(["powershell.exe", "-c", f"(New-Object Media.SoundPlayer \"{wav_file}\").PlaySync();"])
     subprocess.run(["powershell.exe", "-ExecutionPolicy", "RemoteSigned", "-Command", f"New-BurntToastNotification -Text \"COUNTDOWN STATUS\", \"{keep_only_safe_chars(status)}\""])
 
+class OfflineCountdownNotifier(CountdownNotifier):
+
+    def __init__(self):
+        super().__init__()
+
+    def on_status_change(self, online_status):
+        send_notification("notification.wav", online_status)
+        print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%M %p')}] COUNTDOWN STATUS: {online_status}")
+
+#class OnlineCountdownNotifier(CountdownNotifier):
+#    def __init__(self):
+#        super().__init__()
+#
+#Q
+
+
 def main():
     prev_online_status = ""
-
-    exception_sleep_time = 15
-
-    while True:
-        try:
-            r = requests.get(CHADSOFT_STATUS_URL)
-            if r.status_code == 200:
-                if r.text != "":
-                    response_text = r.text
-                    match_obj = countdown_status_regex.search(response_text)
-                    if not match_obj:
-                        print("Warning: Could not find online status message.")
-                    else:
-                        online_status = match_obj.group(1)
-                        if online_status != prev_online_status:
-                            send_notification("notification.wav", online_status)
-                            prev_online_status = online_status
-                            print(f"[{datetime.datetime.now().strftime('%Y-%m-%d %I:%m %p')}] COUNTDOWN STATUS: {online_status}")
-
-                    sleep_time = 180
-                else:
-                    print(f"Chadsoft status returned empty!")
-                    sleep_time = 30
-            else:
-                print(f"chadsoft returned {r.status_code}: {r.reason}")
-                sleep_time = 180
-
-            exception_sleep_time = 15
-            time.sleep(sleep_time)
-        except Exception as e:
-            print(f"Exception occurred: {e}\n{''.join(traceback.format_tb(e.__traceback__))}\nSleeping for {exception_sleep_time} seconds now.")
-            time.sleep(exception_sleep_time)
-            exception_sleep_time *= 2
-            if exception_sleep_time > 1000:
-                print("Exception happened too many times! Exiting.")
-                return
+    cd_notifier = OfflineCountdownNotifier()
+    cd_notifier.wait_status_change()
 
 if __name__ == "__main__":
     main()
